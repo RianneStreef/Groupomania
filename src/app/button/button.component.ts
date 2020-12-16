1a8a64ef182d5d79bf6bd135ac8a4f0bbf1d6ebd
Martin Kruger <martin@martinkruger.com>
2020-12-16 14:21:55 +0200

    Session finished

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,17 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent implements OnInit {
-  
-  buttonText1 = "Sign in";
+  @Input() buttonText: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,47 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
 
-  constructor() { }
+/* function fnThatIsFake() {
+  // Flag is just a boolean in computer science
+  //    that marks something
+  const age = 50;
 
-  ngOnInit(): void {
+  if (age > 18) {
+    return 'Legal drinking age'
+  } else {
+    return 'Not permitted'
   }
 
+  // Ternary Operator:
+              // Condition    // True part          // False
+  const test = age > 18 ? 'Legal drinking age' : 'Not permitted'
+
+  // Can't have this:
+
+  if (age > 18) {
+    return 'Legal drinking age';
+  } else if (age > 21) {
+    return 'Legal driving age';
+  } else {
+    return 'Not permitted'
+  }
+} */
+export class HeaderComponent implements OnInit {
+  isLoggedIn = false;
+  name = 'Rianne';
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  // Functions ALWAYS has a verb
+  changeLoginStatus() {
+    this.isLoggedIn = !this.isLoggedIn;
+  }
+
+  getColor() {
+    return this.isLoggedIn ? 'green' : 'red';
+  }
 }
