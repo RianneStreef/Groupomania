41b8020f189a395a44701df5254d4631dd791b7c
RianneStreef <riannestreef@gmail.com>
2020-12-20 16:22:05 +0100

    copy

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -3,15 +3,22 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent implements OnInit {
-  
+  @Input() buttonStyle: string;
+
   @Input() buttonText: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getClass() {
+    if (this.buttonStyle === 'primary') {
+      return 'btn-primary';
+    } else if (this.buttonStyle === 'secondary') {
+      return 'btn-secondary';
+    }
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
